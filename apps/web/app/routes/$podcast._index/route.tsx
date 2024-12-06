b3fba26e3e8619d3defd4b903a8fef590c3ecb7e
coji <coji@techtalk.jp>
2024-12-07 00:10:12 +0900

    feat: refactor episode card layout; replace link wrapper with icon for improved navigation and maintainability

--- apps/web/app/routes/$podcast._index/route.tsx
+++ apps/web/app/routes/$podcast._index/route.tsx
@@ -1,6 +1,6 @@
 import { format } from 'date-fns/format'
 import { ja } from 'date-fns/locale'
-import { ChevronRightIcon } from 'lucide-react'
+import { ChevronRightIcon, LinkIcon } from 'lucide-react'
 import { data, Link } from 'react-router'
 import {
   Button,
@@ -59,85 +59,74 @@ export default function PodcastIndex({
 
       <Stack className="gap-8">
         {episodes.map((episode) => (
-          <Link
-            to={`${episode.id}`}
-            key={episode.id}
-            className="mx-auto max-w-lg"
-          >
-            <Card key={episode.id}>
-              <CardHeader>
-                <HStack className="gap-4">
-                  <div className="text-xl font-medium">
-                    # {episode.episodeNumber}
-                  </div>
-                  <Separator className="h-8" orientation="vertical" />
-                  <div className="flex-1">
-                    <CardDescription className="text-xs">
-                      {episode.publishedAt &&
-                        format(episode.publishedAt, 'yyyy年MM月dd日(ccc)', {
-                          locale: ja,
-                        })}
-                    </CardDescription>
-                    <CardTitle>{episode.title}</CardTitle>
-                  </div>
-                </HStack>
-              </CardHeader>
-              <CardContent>
-                <Stack>
-                  <div className="text-muted-foreground text-sm">
-                    {episode.description}
-                  </div>
+          <Card key={episode.id}>
+            <CardHeader>
+              <HStack className="gap-4">
+                <div className="text-xl font-medium">
+                  # {episode.episodeNumber}
+                </div>
+                <Separator className="h-8" orientation="vertical" />
+                <div className="flex-1">
+                  <CardDescription className="text-xs">
+                    {episode.publishedAt &&
+                      format(episode.publishedAt, 'yyyy年MM月dd日(ccc)', {
+                        locale: ja,
+                      })}
+                  </CardDescription>
+                  <CardTitle>{episode.title}</CardTitle>
+                </div>
 
-                  {episode.audioUrl && (
-                    // biome-ignore lint/a11y/useKeyWithClickEvents: <explanation>
-                    <audio
-                      controls
-                      src={episode.audioUrl}
-                      className="w-full"
-                      onClick={(e) => e.stopPropagation()}
-                    />
-                  )}
+                <Link
+                  to={`${episode.id}`}
+                  key={episode.id}
+                  className="mx-auto max-w-lg"
+                >
+                  <LinkIcon size="16" />
+                </Link>
+              </HStack>
+            </CardHeader>
+            <CardContent>
+              <Stack>
+                <div className="text-muted-foreground text-sm">
+                  {episode.description}
+                </div>
 
-                  <Collapsible>
-                    <CollapsibleTrigger asChild className="group">
-                      <Button
-                        variant="link"
-                        size="sm"
-                        onClick={(e) => {
-                          console.log('hogehoge')
-                          e.stopPropagation()
-                        }}
-                      >
-                        ショーノート
-                        <ChevronRightIcon
-                          size="16"
-                          className="transition-transform duration-200 group-data-[state=open]:rotate-90"
-                        />
-                      </Button>
-                    </CollapsibleTrigger>
-                    <CollapsibleContent>
-                      <Stack>
-                        {episode.EpisodeSources.map((source) => {
-                          const entry = source.RssEntry
-                          return (
-                            <a
-                              key={entry.id}
-                              href={entry.link}
-                              target="_blank"
-                              rel="noreferrer"
-                              className="ml-8 mr-auto text-sm text-blue-500 hover:underline"
-                            >
-                              <h3>{entry.title}</h3>
-                            </a>
-                          )
-                        })}
-                      </Stack>
-                    </CollapsibleContent>
-                  </Collapsible>
-                </Stack>
-              </CardContent>
-            </Card>
-          </Link>
+                {episode.audioUrl && (
+                  <audio controls src={episode.audioUrl} className="w-full" />
+                )}
+
+                <Collapsible>
+                  <CollapsibleTrigger asChild className="group">
+                    <Button variant="link" size="sm">
+                      ショーノート
+                      <ChevronRightIcon
+                        size="16"
+                        className="transition-transform duration-200 group-data-[state=open]:rotate-90"
+                      />
+                    </Button>
+                  </CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <Stack>
+                      {episode.EpisodeSources.map((source) => {
+                        const entry = source.RssEntry
+                        return (
+                          <a
+                            key={entry.id}
+                            href={entry.link}
+                            target="_blank"
+                            rel="noreferrer"
+                            className="ml-8 mr-auto text-sm text-blue-500 hover:underline"
+                          >
+                            <h3>{entry.title}</h3>
+                          </a>
+                        )
+                      })}
+                    </Stack>
+                  </CollapsibleContent>
+                </Collapsible>
+              </Stack>
+            </CardContent>
+          </Card>
         ))}
       </Stack>
     </Stack>
