4dcd5c1afb1b6450b8a029978bfa6639844bfdfb
coji <coji@techtalk.jp>
2024-12-09 11:19:08 +0900

    style: reorder class names for improved readability and consistency across UI components

--- apps/web/app/components/ui/separator.tsx
+++ apps/web/app/components/ui/separator.tsx
@@ -4,7 +4,7 @@ import * as React from 'react'
 import { cn } from '~/lib/utils'
 
 const Separator = React.forwardRef<
-  React.ElementRef<typeof SeparatorPrimitive.Root>,
+  React.ComponentRef<typeof SeparatorPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
 >(
   (
@@ -16,7 +16,7 @@ const Separator = React.forwardRef<
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        'bg-border shrink-0',
+        'shrink-0 bg-border',
         orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]',
         className,
       )}
