bc97bf1a6129ce1a339db00e45a6e810d03044aa
coji <coji@techtalk.jp>
2024-12-12 08:22:01 +0900

    refactor: remove unused files and exports from clerk-react-router package

--- packages/clerk-react-router/src/client/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './ReactRouterClerkProvider';
-export { ClerkApp } from './ClerkApp';
-export type { WithClerkState } from './types';
-export {
-  SignIn,
-  SignUp,
-  OrganizationProfile,
-  UserProfile,
-} from './uiComponents';
--- packages/clerk-react-router/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './client';
-
-// Override Clerk React error thrower to show that errors come from @clerk/remix
-import { setErrorThrowerOptions } from '@clerk/clerk-react/internal';
-setErrorThrowerOptions({ packageName: 'clerk-react-router' });
